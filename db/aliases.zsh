723983b1616cd4e0d18982055f7d09ddd46e9775
Dan Fleming <dan@dflems.com>
2013-01-15 10:18:00 -0500

    Added mongo aliases, modified redis config

--- db/aliases.zsh
+++ db/aliases.zsh
@@ -1,11 +1,12 @@
 alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pg-down='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
 
-alias redis-up="redis-server $ZSH/db/redis.conf > /dev/null &"
+alias redis-up="redis-server $ZSH/db/redis.conf"
 alias redis-down='killall redis-server'
 
 alias mysql-up='mysql.server start'
 alias mysql-down='mysql.server stop'
 
-alias mongo-up='mongod --fork --logpath /usr/local/var/log/mongodb.log --logappend'
-alias mongo-down='kill -2 $(cat /usr/local/var/mongodb/mongod.lock)' #safe
+# default homebrew mongo log location
+alias mongo-up='mongod --fork --logappend'
+alias mongo-down='killall -2 mongod' #safe
