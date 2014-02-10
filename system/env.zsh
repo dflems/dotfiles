d63348aceaca6015715cac2de3cf8294e4e60cd5
Dan Fleming <dflems@spotify.com>
2014-02-10 10:02:38 -0500

    Python & virtualenv

--- system/env.zsh
+++ system/env.zsh
@@ -8,3 +8,9 @@ fi
 if [ -f /usr/libexec/java_home ]; then
   export JAVA_HOME=$(/usr/libexec/java_home)
 fi
+
+# Python w/ virtualenv wrapper
+ export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/bin/python
+ if [ -f $(brew --prefix)/bin/virtualenvwrapper_lazy.sh ]; then
+   . $(brew --prefix)/bin/virtualenvwrapper_lazy.sh
+ fi
