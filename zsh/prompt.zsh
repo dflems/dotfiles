336a882750e93ab65215a446d378d8a12caeed46
Dan Fleming <dan@dflems.com>
2015-05-29 01:12:44 -0400

    Enhance the prompt
    based on pure by @sindresorhus
    https://github.com/sindresorhus/pure

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,70 +1,54 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
+# output git branch
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  # bail out if we don't have a branch name
+  [[ "$vcs_info_msg_0_" == "" ]] && return
+  # dirty check (bail if command fails)
+  command test -n "$(git status --porcelain 2>/dev/null | tail -n 1)" &>/dev/null
+  (($? == 0)) && git_dirty="*"
+  # output colored branch name
+  [[ "$git_dirty" == "" ]] && git_color=green || git_color=red
+  echo "%F{$git_color}${vcs_info_msg_0_}${git_dirty}%f"
 }
 
-git_dirty() {
-  branch_name=$(git_prompt_info)
-  state=$($git status --porcelain 2>/dev/null | tail -n 1)
-
-  if [[ ! -z "$branch_name" || ! -z "$state" ]]; then
-    if [[ -z "$branch_name" ]]; then branch_name='UNTRACKED'; fi
-    if [[ "$state" = "" ]]; then
-      echo "on %{$fg_bold[green]%}${branch_name}%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}${branch_name}%{$reset_color%}"
-    fi
-  else
-    echo ''
-  fi
+# prints up arrow if unpushed commits. down arrow if unpulled.
+git_arrows() {
+  command git rev-parse --abbrev-ref @'{u}' &>/dev/null || return
+  local arrows=""
+  (( $(command git rev-list --right-only --count HEAD...@'{u}' 2>/dev/null) > 0 )) && arrows='⇣'
+  (( $(command git rev-list --left-only --count HEAD...@'{u}' 2>/dev/null) > 0 )) && arrows+='⇡'
+  [[ "$arrows" != "" ]] && echo " ${arrows}"
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
+# render the prompt
+render_prompt() {
+  # prompt begins with path
+  local prompt="%F{blue}%~%f"
+  # git branch and status
+  prompt+="$(git_branch)"
+  # git pull/push arrows
+  prompt+="%F{magenta}$(git_arrows)%f"
+  # yellow cursor. red if last command failed
+  prompt+="\n%(?.%F{yellow}.%F{red})❯%f "
+  # echo
+  echo "$prompt"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+# set up the prompt
+prompt_setup() {
+  autoload -Uz vcs_info
+  zstyle ':vcs_info:*' enable git
+  zstyle ':vcs_info:*' use-simple true
+  zstyle ':vcs_info:git*' formats ' %b'
+  zstyle ':vcs_info:git*' actionformats ' %b|%a'
+  export PROMPT=$'\n$(render_prompt)'
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if [ "$(which ruby)" = "/usr/bin/ruby" ]; then 
-    echo "system"
-  else
-    echo `ruby -v | awk '{print $2}'`
-  fi
-}
-
-directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-export PROMPT=$'\n%{$fg_bold[magenta]%}%n%{$reset_color%} at %{$fg_bold[yellow]%}%m%{$reset_color%} in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[grey]%}$(ruby_version)%{$reset_color%}"
-}
-
+# run before each command
 precmd() {
+  # update title to hostname
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
+  # update VCS info
+  vcs_info
 }
+
+prompt_setup
