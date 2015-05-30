c840771dbcc284d2dc2455a9b302a345a8722a21
Dan Fleming <dan@dflems.com>
2015-05-30 00:46:49 -0400

    editor

---
+++ system/editor.zsh
@@ -0,0 +1,21 @@
+# Set editor if unspecified
+if [[ "$EDITOR" == "" ]] ; then
+  # Use `subl` as the editor if it's on the path
+ if type subl >/dev/null 2>&1; then
+    export EDITOR='subl'
+  else
+    export EDITOR='vi'
+  fi
+fi
+
+# Editor function
+function e() {
+  if [ "$1" = "" ] ; then
+    exec $EDITOR .
+  else
+    exec $EDITOR "$1"
+  fi
+}
+
+# Shortcut to edit dotfiles
+alias edot="e \"$ZSH\""
--- system/env.zsh
+++ system/env.zsh
@@ -1,19 +1,10 @@
-if [[ "$EDITOR" == "" ]] ; then
-  # Use `subl` as the editor if it's on the path
- if type subl >/dev/null 2>&1; then
-    export EDITOR='subl'
-  else
-    export EDITOR='vi'
-  fi
-fi
-
 # Java Home
 if [ -f /usr/libexec/java_home ]; then
   export JAVA_HOME=$(/usr/libexec/java_home)
 fi
 
 # Python w/ virtualenv wrapper
- export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/bin/python
- if [ -f $(brew --prefix)/bin/virtualenvwrapper_lazy.sh ]; then
+export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/bin/python
+if [ -f $(brew --prefix)/bin/virtualenvwrapper_lazy.sh ]; then
   . $(brew --prefix)/bin/virtualenvwrapper_lazy.sh
- fi
+fi
