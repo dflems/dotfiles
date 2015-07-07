11554de6cac5a265259f5e3267c8405343899698
Dan Fleming <dflems@spotify.com>
2015-07-07 15:56:57 -0400

    virtualenvwrapper tweaks

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,12 @@
+if [[ "$EDITOR" == "" ]] ; then
+  # Use `subl` as the editor if it's on the path
+ if type subl >/dev/null 2>&1; then
+    export EDITOR='subl'
+  else
+    export EDITOR='vi'
+  fi
+fi
+
 # Java Home
 if [ -f /usr/libexec/java_home ]; then
   export JAVA_HOME=$(/usr/libexec/java_home)
@@ -6,5 +15,6 @@ fi
 # Python w/ virtualenv wrapper
 export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/bin/python
 if [ -f $(brew --prefix)/bin/virtualenvwrapper_lazy.sh ]; then
+  export WORKON_HOME=$HOME/.virtualenvs
   . $(brew --prefix)/bin/virtualenvwrapper_lazy.sh
 fi
