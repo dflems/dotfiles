a58471ca12126c1de4b42696b8c90bcbf937d02e
Dan Fleming <dan@dflems.com>
2016-08-25 02:02:28 -0400

    homebrew tweaks

---
+++ Brewfile
@@ -0,0 +1,21 @@
+# ruby environment
+brew 'chruby'
+brew 'ruby-install'
+
+# install other useful binaries
+brew 'chisel'
+brew 'git'
+brew 'gnupg'
+brew 'go'
+brew 'hub'
+brew 'jq'
+brew 'ngrep'
+brew 'nmap'
+brew 'node'
+brew 'rename'
+brew 'the_silver_searcher'
+brew 'trash'
+brew 'tree'
+brew 'vim', args: ['override-system-vi']
+brew 'youtube-dl'
+brew 'wget', args: ['enable-iri']
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,50 +1,24 @@
 #!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
 
-# check for Homebrew
-if ! type brew >/dev/null 2>&1; then
-  echo "Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
+
 fi
 
-# install completions into dynamic fpath
-bc_path="$(brew --prefix 2>/dev/null)/Library/Contributions/brew_zsh_completion.zsh"
-if [ -f "$bc_path" ]; then
-  mkdir -p "$HOME/.fpath"
-  ln -sf "$bc_path" "$HOME/.fpath/_brew"
-fi
-
-# echo on
-set -x
-
-# latest 'n' greatest
-brew update
-brew upgrade --all
-
-# install wget with IRI support
-brew install wget --enable-iri
-
-# install more recent versions of some macOS tools
-brew install vim --override-system-vi
-
-# ruby environment
-brew install chruby
-brew install ruby-install
-
-# install other useful binaries
-brew install chisel
-brew install git
-brew install gnupg
-brew install go
-brew install hub
-brew install jq
-brew install ngrep
-brew install nmap
-brew install node
-brew install rename
-brew install the_silver_searcher
-brew install trash
-brew install tree
-brew install youtube-dl
-
-# cleanup
-brew cleanup
+exit 0
