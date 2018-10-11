a0033d049ec00dc2606cc70a0d48ba185ce2e9da
Dan Fleming <dflems@spotify.com>
2018-10-11 16:20:22 -0400

    python stuff

--- python/path.zsh
+++ python/path.zsh
@@ -1,17 +1,13 @@
-BREW_PYTHON_LIBEXEC="$(brew --prefix)/opt/python/libexec/bin"
-if [ -d "$BREW_PYTHON_LIBEXEC" ]; then
-  export PATH="$BREW_PYTHON_LIBEXEC:$PATH"
-
+local MY_PYTHON="$(which python)"
+if [ "$MY_PYTHON" != "/usr/bin/python" ]; then
   # use virtualenv wrapper
   if [ -f $(brew --prefix)/bin/virtualenvwrapper_lazy.sh ]; then
-    export VIRTUALENVWRAPPER_PYTHON="$BREW_PYTHON"
     export VIRTUAL_ENV_DISABLE_PROMPT=1
     export WORKON_HOME=$HOME/.virtualenvs
     . $(brew --prefix)/bin/virtualenvwrapper_lazy.sh
 
     # venv will create a named virtualenv or one based on the current dir
     venv () {
-      virtualenvwrapper_load
       NAME="$1"
       if [ -z "$NAME" ]; then
         NAME=`basename "$PWD"`
