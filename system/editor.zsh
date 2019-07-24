1e697ae99882176df7eb815fb0f788fc3b165e39
Dan Fleming <dflems@spotify.com>
2019-07-24 15:36:57 -0400

    subp is subl but opens projects if they exist

--- system/editor.zsh
+++ system/editor.zsh
@@ -2,7 +2,7 @@
 if [[ "$EDITOR" == "" ]] ; then
   # Use `subl` as the editor if it's on the path
  if type subl >/dev/null 2>&1; then
-    export EDITOR='subl'
+    export EDITOR='subp'
   else
     export EDITOR='vi'
   fi
