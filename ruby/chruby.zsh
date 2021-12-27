57bf4a67800f7f43947d0dbc692e07d4d53dc118
Dan Fleming <dflems@spotify.com>
2021-12-27 12:49:38 -0500

    brew updates

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,4 +1,8 @@
-if [ -f /usr/local/share/chruby/chruby.sh ]; then
+if [ -f /opt/homebrew/share/chruby/chruby.sh ]; then
+  source /opt/homebrew/share/chruby/chruby.sh
+  source /opt/homebrew/share/chruby/auto.sh
+  chruby_auto
+elif [ -f /usr/local/share/chruby/chruby.sh ]; then
   source /usr/local/share/chruby/chruby.sh
   source /usr/local/share/chruby/auto.sh
   chruby_auto
